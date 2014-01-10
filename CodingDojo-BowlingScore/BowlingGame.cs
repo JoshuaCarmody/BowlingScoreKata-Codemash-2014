fa6f214469153796a2e76e2a8ddd3bbaf7d80893
Joshua Carmody <contact@joshuacarmody.com>
2014-01-10 11:53:03 -0500

    Modified throwBall(int) method of BowlingGame to advance the frame as balls are thrown. Separated scoring logic into separate method and expanded a bit.

--- CodingDojo-BowlingScore/BowlingGame.cs
+++ CodingDojo-BowlingScore/BowlingGame.cs
@@ -31,15 +31,31 @@ namespace CodingDojo_BowlingScore
 
         public void throwBall(int pinsHit)
         {
-            gameScore += pinsHit;
+            gameFrames[currentFrame - 1].throwBall(pinsHit);
+
+            if (gameFrames[currentFrame - 1].IsOver)
+            {
+                currentFrame++;
+            }
+
+            RecalculateScore();
+        }
+
+        private void RecalculateScore()
+        {
+            gameScore = 0;
+            foreach (var frame in gameFrames)
+            {
+                gameScore += frame.Score;
+            }
         }
 
         public BowlingGame()
         {
             gameFrames = new BowlingFrame[10];
-            for (int i = 1; i <= 10; i++)
+            for (int i = 0; i < 10; i++)
             {
-                gameFrames[i] = new BowlingFrame(i);
+                gameFrames[i] = new BowlingFrame(i + 1); // Bowling frame numbers are 1-based, array index is 0-based
             }
         }
     }
