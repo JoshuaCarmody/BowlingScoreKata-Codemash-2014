92a82e62873fd6f92902ffa0ad78f4ca4a3187e8
Joshua Carmody <contact@joshuacarmody.com>
2014-01-10 10:23:53 -0500

    Added enough code so that I could run the tests (and some of them fail): * Created BowlingGame class with Score property and throwBall(int) method. * Made score a simple cumulative addition function. * Created GameAlreadyOverException.

--- CodingDojo-BowlingScore-Test/BowlingScoreTests.cs
+++ CodingDojo-BowlingScore-Test/BowlingScoreTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using CodingDojo_BowlingScore;
+
 namespace CodingDojo_BowlingScore_Test
 {
     [TestClass]
---
+++ CodingDojo-BowlingScore/BowlingGame.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingDojo_BowlingScore
+{
+    public class BowlingGame
+    {
+        protected int gameScore = 0;
+
+        public int Score
+        {
+            get
+            {
+                return gameScore;
+            }
+        }
+
+        public void throwBall(int pinsHit)
+        {
+            gameScore += pinsHit;
+        }
+    }
+}
